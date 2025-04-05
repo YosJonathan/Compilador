593bd58d0385b5e352a6adfe2ebf2b6c39730638
Jonathan Estuardo Yos Escobar <78168255+YosJonathan@users.noreply.github.com>
2025-04-04 18:39:34 -0600

    Corrigiendo funcion main declare y declaraciones

--- Compilador/FrontEnd/Gramatica.cs
+++ Compilador/FrontEnd/Gramatica.cs
@@ -18,74 +18,81 @@ public class Gramatica : Grammar
     /// </summary>
     public Gramatica()
     {
-        // Definición de los terminales
-        var include = new KeyTerm("#include", "#include");
+        // Terminales
+        var include = ToTerm("#include");
+        var tipoInt = ToTerm("int");
+        var tipoFloat = ToTerm("float");
+        var tipoChar = ToTerm("char");
+        var tipoVoid = ToTerm("void");
+        var operadorAsignacion = ToTerm("=");
+        var operadorSuma = ToTerm("+");
+        var operadorScanf = ToTerm("scanf");
+        var operadorPrintf = ToTerm("printf");
+        var puntoYComa = ToTerm(";");
+        var parentesisAbrir = ToTerm("(");
+        var parentesisCerrar = ToTerm(")");
+        var coma = ToTerm(",");
+        var ampersand = ToTerm("&");
+        var llaveAbrir = ToTerm("{");
+        var llaveCerrar = ToTerm("}");
+
         var identificador = new IdentifierTerminal("identificador");
-        var tipoInt = new KeyTerm("int", "int");
-        var tipoFloat = new KeyTerm("float", "float");
-        var tipoChar = new KeyTerm("char", "char");
-        var tipoVoid = new KeyTerm("void", "void");
-        var operadorAsignacion = new KeyTerm("=", "=");
-        var operadorSuma = new KeyTerm("+", "+");
-        var operadorScanf = new KeyTerm("scanf", "scanf");
-        var operadorPrintf = new KeyTerm("printf", "printf");
-        var puntoYComa = new KeyTerm(";", "puntoYComa");
-        var parentesisAbrir = new KeyTerm("(", "(");
-        var parentesisCerrar = new KeyTerm(")", ")");
-        var coma = new KeyTerm(",", "coma");
-        var ampersand = new KeyTerm("&", "&");
-
-        // Crear un Terminal que maneje la cadena entre < y > para #include
         var archivoInclude = new RegexBasedTerminal("archivoInclude", "<[a-zA-Z0-9_\\.]+>");
-
-        // Definir el número entero como un literal de número
         var numeroEntero = new NumberLiteral("numeroEntero");
-
-        // Definir las comillas dobles para cadenas de texto
-        var comillas = new KeyTerm("\"", "\"");
         var cadena = new StringLiteral("cadena", "\"");
 
-        // Regla para la directiva #include
+        // No terminales
         var includeDirectiva = new NonTerminal("includeDirectiva");
+        var declaracionVariable = new NonTerminal("declaracionVariable");
+        var expresion = new NonTerminal("expresion");
+        var funcion = new NonTerminal("funcion");
+        var declaracionesEnBloque = new NonTerminal("declaracionesEnBloque");
+        var bloque = new NonTerminal("bloque");
+        var funcionMain = new NonTerminal("funcionMain");
+        var programa = new NonTerminal("programa");
+        var listaIncludes = new NonTerminal("listaIncludes");
+        var listaParametrosPrintf = new NonTerminal("listaParametrosPrintf");
+        var parametroScanf = new NonTerminal("parametroScanf");
+        var listaParametrosScanf = new NonTerminal("listaParametrosScanf");
+
+        parametroScanf.Rule = ampersand + identificador;
+        listaParametrosScanf.Rule = MakePlusRule(listaParametrosScanf, coma, parametroScanf);
+
+
+        // Reglas
+        listaParametrosPrintf.Rule = MakePlusRule(listaParametrosPrintf, coma, identificador);
+
+        listaIncludes.Rule = MakeStarRule(listaIncludes, includeDirectiva);
+        programa.Rule = listaIncludes + funcionMain;
+
+        funcionMain.Rule = tipoInt + ToTerm("main") + parentesisAbrir + parentesisCerrar + bloque | includeDirectiva;
+
+        bloque.Rule = llaveAbrir + declaracionesEnBloque + llaveCerrar;
+
+
         includeDirectiva.Rule = include + archivoInclude;
 
-        // Regla para las expresiones
-        var expresion = new NonTerminal("expresion");
-        expresion.Rule = identificador + operadorAsignacion + identificador
-                        | identificador + operadorAsignacion + numeroEntero
-                        | identificador + operadorSuma + identificador;
+        declaracionVariable.Rule = (tipoInt | tipoFloat | tipoChar) + identificador + puntoYComa;
 
-        // Regla para la función printf
-        var funcion = new NonTerminal("funcion");
+        expresion.Rule = identificador + operadorAsignacion + numeroEntero + puntoYComa
+                       | identificador + operadorAsignacion + identificador + puntoYComa
+                       | identificador + operadorSuma + identificador + puntoYComa;
+
         funcion.Rule = operadorPrintf + parentesisAbrir + cadena + parentesisCerrar + puntoYComa
-                      | operadorScanf + parentesisAbrir + cadena + coma + ampersand + identificador + parentesisCerrar + puntoYComa;
+             | operadorPrintf + parentesisAbrir + cadena + coma + listaParametrosPrintf + parentesisCerrar + puntoYComa
+             | operadorScanf + parentesisAbrir + cadena + coma + ampersand + listaParametrosScanf + parentesisCerrar + puntoYComa;
 
-        // Regla para la declaración de variables
-        var declaracionVariable = new NonTerminal("declaracionVariable");
-        declaracionVariable.Rule = tipoInt + identificador + coma + identificador + coma + identificador + puntoYComa
-                                  | tipoFloat + identificador + coma + identificador + coma + identificador + puntoYComa
-                                  | tipoChar + identificador + coma + identificador + coma + identificador + puntoYComa
-                                  | tipoInt + identificador + puntoYComa
-                                  | tipoFloat + identificador + puntoYComa
-                                  | tipoChar + identificador + puntoYComa;
 
-        // Regla para declaraciones dentro de un bloque
-        var declaracionesEnBloque = new NonTerminal("declaracionesEnBloque");
-        declaracionesEnBloque.Rule = MakePlusRule(declaracionesEnBloque, declaracionVariable | expresion | funcion);
+        declaracionesEnBloque.Rule = MakeStarRule(declaracionesEnBloque,funcion | declaracionVariable | expresion);
 
-        // Regla para el bloque de código dentro de una función
-        var bloque = new NonTerminal("bloque");
-        bloque.Rule = "{" + declaracionesEnBloque + "}";
+        // Establecer el punto de entrada
+        this.Root = programa;
 
-        // Regla para la función main (especificando que 'main' es un terminal explícito)
-        var funcionMain = new NonTerminal("funcionMain");
-        funcionMain.Rule = tipoInt + "main" + parentesisAbrir + parentesisCerrar + bloque;
+        // Marcar símbolos de puntuación para que no aparezcan en el árbol
+        MarkPunctuation(";", "(", ")", ",", "&", "{", "}", "#include");
 
-        // La raíz de la gramática ahora incluye #include y la función main
-        Root = new NonTerminal("Root");
-        Root.Rule = MakePlusRule(Root, includeDirectiva | funcionMain );
-
-        // Definir qué hacer con los espacios en blanco
-        this.LanguageFlags = LanguageFlags.CreateAst;
+        // Marcar palabras reservadas
+        MarkReservedWords("int", "float", "char", "void", "printf", "scanf");
     }
+
 }
